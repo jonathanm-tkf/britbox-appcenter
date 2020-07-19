3738f33407037a016e9ceeb6c667d7a93a89ee0b
jonathanmachado <jonathanmachado48@gmail.com>
2020-07-19 19:34:48 -0300

    design tabs component dynamic

--- src/components/TabsComponent/styles.ts
+++ src/components/TabsComponent/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { TabBar as TabBarC } from 'react-native-tab-view';
 import { ThemeState } from '@store/modules/theme/types';
+import { rgba } from 'polished';
 
 export const Container = styled.View`
   background-color: white;
@@ -9,16 +10,48 @@ export const Container = styled.View`
 `;
 
 export const TabBar = styled(TabBarC).attrs((props: ThemeState) => ({
-  indicatorStyle: { backgroundColor: props.theme.SECONDARY_COLOR_LIGHT },
+  indicatorStyle: {
+    backgroundColor: props.theme.SECONDARY_COLOR_LIGHT,
+    width: 0,
+  },
   activeColor: props.theme.SECONDARY_COLOR_LIGHT,
+  inactiveColor: rgba(props.theme.PRIMARY_FOREGROUND_COLOR, 0.6),
+  tabStyle: {
+    width: 'auto',
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
+    borderColor: rgba(props.theme.PRIMARY_FOREGROUND_COLOR, 0.1),
+  },
 }))`
   background-color: ${(props: ThemeState) => props.theme.PRIMARY_COLOR};
 `;
 
+export const TabWrapper = styled.View``;
+
 interface TabLabel {
   color: string;
+  focused: boolean;
 }
 export const TabLabel = styled.Text`
-  font-family: ${(props: ThemeState) => props.theme.PRIMARY_FONT_FAMILY_MEDIUM};
   color: ${(props: TabLabel & ThemeState) => props.color};
+  font-size: 16px;
+  ${(props: TabLabel & ThemeState) => {
+    return props.focused
+      ? `font-family: ${props.theme.PRIMARY_FONT_FAMILY_MEDIUM}`
+      : `font-family: ${props.theme.PRIMARY_FONT_FAMILY}`;
+  }};
+`;
+
+export const IndicatorWrapper = styled.View`
+  width: 100%;
+  position: absolute;
+  bottom: -14px;
+  left: 0;
+  right: 0;
+`;
+export const Indicator = styled.View`
+  background-color: ${(props: ThemeState) => props.theme.SECONDARY_COLOR_LIGHT};
+  width: 30px;
+  height: 3px;
+  align-self: center;
 `;
