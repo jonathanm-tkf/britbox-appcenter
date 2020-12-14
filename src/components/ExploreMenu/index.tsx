55fc5a10b73baa7fc0812f80a91ccb0e0565a979
Cristian García <cristiang@takeoffmedia.com>
2020-12-14 12:26:57 -0300

    fix big screens "Explore"'s tab titles position

--- src/components/ExploreMenu/index.tsx
+++ src/components/ExploreMenu/index.tsx
@@ -10,6 +10,7 @@ import {
   TabHeader,
   TabHeaderItem,
   TabHeaderItemText,
+  BigScreenTabHeaderItemTextWrapper,
   TabHeaderItemIndicator,
   LinksWrapper,
   TabContent,
@@ -84,6 +85,7 @@ const ExploreMenu = ({ data, onPress }: Props) => {
                 isTablet() && orientation === 'LANDSCAPE' && headerIndex === 0 ? '10%' : undefined
               }
             >
+<<<<<<< HEAD
               {headerItem.label}
             </TabHeaderItemText>
           </TabHeaderItem>
@@ -100,13 +102,68 @@ const ExploreMenu = ({ data, onPress }: Props) => {
               <TabButton
                 key={`${headerItem.label}_${headerIndex.toString()}`}
                 onPress={() => onPress(link as Header)}
+=======
+              {item.label}
+            </HeaderBottom>
+          );
+        })}
+      </HeaderWrapper>
+      {isTablet() || orientation === 'LANDSCAPE' ? (
+        <TabHeader>
+          {dataMenu.map((item: Header, index: number) => (
+            <TabHeaderItem key={item.label.toString() + index.toString()} disabled bigScreen>
+              <BigScreenTabWrapper>
+                <BigScreenTabHeaderItemTextWrapper>
+                  <TabHeaderItemText active={false}>{item.label}</TabHeaderItemText>
+                </BigScreenTabHeaderItemTextWrapper>
+                <TabContent key={item.label.toString()} active bigScreen>
+                  {(item.children || []).map((link, _index) => (
+                    <TabButton
+                      key={`${item.label}_${_index.toString()}`}
+                      onPress={() => onPress(link as Header)}
+                    >
+                      <TabButtonText>{link.label}</TabButtonText>
+                    </TabButton>
+                  ))}
+                </TabContent>
+              </BigScreenTabWrapper>
+            </TabHeaderItem>
+          ))}
+        </TabHeader>
+      ) : (
+        <>
+          <TabHeader>
+            {dataMenu.map((item: Header, index: number) => (
+              <TabHeaderItem
+                key={item.label.toString() + index.toString()}
+                onPress={() => changeTab(item.label)}
+>>>>>>> e7ca39b... fix big screens "Explore"'s tab titles position
               >
                 <TabButtonText>{link.label}</TabButtonText>
               </TabButton>
             ))}
+<<<<<<< HEAD
           </TabContent>
         ))}
       </LinksWrapper>
+=======
+          </TabHeader>
+          {dataMenu.map((item: Header) => (
+            <TabContent key={item.label.toString()} active={active === item.label}>
+              {(item.children || []).map((link, index) => (
+                <TabButton
+                  key={`${item.label}
+                  ${index.toString()}`}
+                  onPress={() => onPress(link as Header)}
+                >
+                  <TabButtonText>{link.label}</TabButtonText>
+                </TabButton>
+              ))}
+            </TabContent>
+          ))}
+        </>
+      )}
+>>>>>>> e7ca39b... fix big screens "Explore"'s tab titles position
     </Container>
   );
 };
