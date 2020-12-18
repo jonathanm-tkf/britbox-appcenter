bea27ca752f383366de02313a10ea3ec7968ee9c
Cristian García <cristiang@takeoffmedia.com>
2020-12-18 12:29:46 -0300

    90% instead 80%

--- src/utils/dimension.ts
+++ src/utils/dimension.ts
@@ -57,17 +57,9 @@ export const percentageHeight = (percentage: number) => {
 /**
  * Tablet-Screen-Width
  * A few screens have the exact same width on tablets
-<<<<<<< HEAD
  * @returns {number} 90% of the screen width
  */
 export const getTabletScreenWidth = (): number => {
   const { width: screenWidth, height: screenHeight } = getDimensions();
   return Math.min(screenWidth, screenHeight) * 0.9;
-=======
- * @returns {number} 80% of the screen width
- */
-export const getTabletScreenWidth = (): number => {
-  const { width: screenWidth, height: screenHeight } = getDimensions();
-  return Math.min(screenWidth, screenHeight) * 0.8;
->>>>>>> e4da09c... create a function to calculate 80% of the screen width
 };
