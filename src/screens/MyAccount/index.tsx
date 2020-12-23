fe656aa6fa48595791356b56f75401d27c493295
Cristian García <cristiang@takeoffmedia.com>
2020-12-23 12:28:47 -0300

    fix default newsletter check status

--- src/screens/MyAccount/index.tsx
+++ src/screens/MyAccount/index.tsx
@@ -790,7 +790,7 @@ export default function MyAccount() {
 
   const NewsletterRoute = () => {
     const [isNewsletters, setIsNewsletters] = useState(
-      Boolean(user?.profile?.isAlertNotificationEmail) || false
+      user?.profile?.isAlertNotificationEmail === 'true'
     );
     const [loading, setLoading] = useState(false);
     const [isSuccess, setIsSuccess] = useState(false);
