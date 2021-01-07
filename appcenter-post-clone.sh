34319ac99436e96a3cb4446ce4f745543f7884a5
Jonathan Machado <jonathanm@takeoffmedia.com>
2021-01-07 20:31:58 -0300

    test cocoapods

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,3 +1,5 @@
+sudo gem update --system
+
 COCOAPODS_VER=sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock
 if test -z "$COCOAPODS_VER"
 then
@@ -5,5 +7,5 @@ COCOAPODS_VER=1.10.0
 fi
 
 echo "Installing CocoaPods version $COCOAPODS_VER"
-sudo gem install cocoapods -v $COCOAPODS_VER
+sudo gem install -n /usr/local/bin cocoapods
 
