6b987d3c75040bfb84b62ad750ed75e9235ba40b
Cristian García <cristiang@takeoffmedia.com>
2021-01-19 10:25:53 -0300

    fix first screen orientation on auth screen

--- src/utils/orientation.ts
+++ src/utils/orientation.ts
@@ -1,18 +1,27 @@
 import { useEffect, useCallback, useState } from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, Platform } from 'react-native';
 import OrientationLocker, {
   OrientationType as LockerOrientationType,
 } from 'react-native-orientation-locker';
 
 export type Orientation = 'PORTRAIT' | 'LANDSCAPE';
 
-/**
- * Returns true if the screen is in portrait mode
- */
-const isPortrait = () => {
-  const dim = Dimensions.get('window');
-  return dim.height >= dim.width;
+const parseOrientationType = (orientation: LockerOrientationType): Orientation => {
+  // PORTRAIT has preference
+  if (orientation === 'LANDSCAPE-RIGHT' || orientation === 'LANDSCAPE-LEFT') {
+    return 'LANDSCAPE';
+  }
+
+  return 'PORTRAIT';
 };
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
+let initialOrientation: Orientation = screenHeight >= screenWidth ? 'PORTRAIT' : 'LANDSCAPE';
+
+// The strangest behaviour I've ever seen, but this fix works...
+if (Platform.OS === 'android' && screenHeight >= screenWidth) {
+  initialOrientation = 'PORTRAIT';
+}
 
 /**
  * A React Hook which updates when the orientation changes
@@ -20,18 +29,12 @@ const isPortrait = () => {
  */
 export function useOrientation(): Orientation {
   // State to hold the connection status
-  const [orientation, setOrientation] = useState<Orientation>(
-    isPortrait() ? 'PORTRAIT' : 'LANDSCAPE'
-  );
+  const [orientation, setOrientation] = useState<Orientation>(initialOrientation);
 
   const onOrientationDidChange = useCallback(() => {
     OrientationLocker.getOrientation((newOrientation: LockerOrientationType) => {
-      // PORTRAIT has preference
-      if (newOrientation === 'LANDSCAPE-RIGHT' || newOrientation === 'LANDSCAPE-LEFT') {
-        setOrientation('LANDSCAPE');
-      } else {
-        setOrientation('PORTRAIT');
-      }
+      const parsed = parseOrientationType(newOrientation);
+      setOrientation(parsed);
     });
   }, []);
 
@@ -41,7 +44,7 @@ export function useOrientation(): Orientation {
     return () => {
       OrientationLocker.removeOrientationListener(onOrientationDidChange);
     };
-  });
+  }, [onOrientationDidChange]);
 
   return orientation;
 }
